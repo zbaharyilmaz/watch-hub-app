a7a4ec9c7cbefa6be11f1a0d6c0235291123598f
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-03-16 23:45:20 +0300

    Initialize Next.js project with basic configuration and setup

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="relative h-screen w-full">
+      <div className="bg-black w-full h-full bg-opacity-50">
+        <div>
+          <h1>Watch, Discover, Enjoy – Entertainment at Your Fingertips</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
